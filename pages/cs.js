3d1e39877f931be163d729126e058cb9601f0710
like115 <258861342@qq.com>
2022-01-25 11:02:37 +0800

    dd

--- pages/cs.js
+++ pages/cs.js
@@ -3,3 +3,8 @@ var aa = "ffff" ;
 var chr = typeof(aa);
 console.log("console.log 命令可以直接在node环境中运行");
 console.log(chr) ;
+console.log('------------------------------');
+var box = new Object();
+box.name = "like" ;
+box.age = 20 ;
+console.log(box);
