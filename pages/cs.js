6c1b45b00e3e972dfd32ce11047b00bb8cfefe5e
like115 <258861342@qq.com>
2022-06-16 14:56:18 +0800

    20220616-2

--- pages/cs.js
+++ pages/cs.js
@@ -4,14 +4,10 @@ var chr = typeof(aa);
 console.log("console.log 命令可以直接在node环境中运行");
 console.log('------------------------------');
 
-var value = 1;
-function foo() {
-  console.log("变量value   " + value);
+function Student(props) {
+   this.name = props.name || 'Unnamed';
 }
-function bar() {
-  var value = 2;
-  foo();
+
+Student.prototype.hello = function () {
+   alert('Hello, ' + this.name + '!');
 }
-bar();
-
-
